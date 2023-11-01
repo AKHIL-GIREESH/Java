ce263412df4713dac9231ef7a784eb0133d97f17
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-01 22:11:12 +0530

    super...

---
+++ Super.java
@@ -0,0 +1,31 @@
+class A{
+    A(){
+        System.out.println("A"); 
+    }
+
+    A(int a){
+        System.out.println("Aint");
+    }
+}
+
+class B extends A{
+    int a  = 10;
+    B(){
+        super(10);
+        System.out.println("B");
+    }
+} 
+
+
+public class Super {
+    public static void main(String a[]){
+        B b = new B();
+
+        B a1 = b;
+        a1.a = 5;
+        System.out.println(b.a);
+
+    }   
+}
+
+
