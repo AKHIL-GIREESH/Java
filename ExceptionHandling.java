7669c992d2f638b5c0ffb24f666d81d938b01c67
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-14 19:55:05 +0530

    throws

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,11 +1,11 @@
 public class ExceptionHandling {
-    public static void main(String a[]){
+    public static void main(String a[]) throws ArithmeticException{
         int aae=1,bee=0;
-        try{
+        // try{
             System.out.println(aae/bee);
-        }catch(Exception e){
-            System.out.println("Division by 0 bruh");
-        }
+        // }catch(Exception e){
+        //     System.out.println("Division by 0 bruh");
+        // }
         System.out.println("idk");
     }
 }
