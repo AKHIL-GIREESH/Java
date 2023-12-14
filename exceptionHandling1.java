af3b77b7cacce422c7a5e4e2443df071a16605a0
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-12-14 19:09:00 +0530

    Exception Handling

--- exceptionHandling1.java
+++ exceptionHandling1.java
@@ -1,12 +1,11 @@
-import java.io.*;
-
 public class exceptionHandling1 {
     public static void main(String ar[]){
-        int a = 1;
+        
         try{
-            int b;
+            
             try{
-                b = 1/(a-a);
+
+
             }catch(Exception e){
                 System.out.println(e);
             }
